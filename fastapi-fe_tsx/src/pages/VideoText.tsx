a0b613b3212dcb8cf46f0229eb291dad8d6af8f0
coil <michal.vasulka@gmail.com>
2023-02-23 23:20:41 +0100

    [video] streamlined tabs for Original and Translation

--- fastapi-fe_tsx/src/pages/VideoText.tsx
+++ fastapi-fe_tsx/src/pages/VideoText.tsx
@@ -170,44 +170,74 @@ function VideoText() {
 */}
 
       <div className="flex">
+
+
         <div className="w-2/4 p-4">
 
 
-          <div className="flex items-start">
-            <ul className="nav nav-tabs flex flex-col flex-wrap list-none border-b-0 pl-0 mr-4" id="tabs-tabVertical"
-              role="tablist">
-              <li className="nav-item flex-grow text-center" role="presentation">
-                <a href="#tabs-homeVertical" className="nav-link block font-medium text-xs leading-tight uppercase border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent active"
-                  id="tabs-home-tabVertical" data-bs-toggle="pill" data-bs-target="#tabs-homeVertical" role="tab"
-                  aria-controls="tabs-homeVertical" aria-selected="true">Original</a>
-              </li>
-              <li className="nav-item flex-grow text-center" role="presentation">
-                <a href="#tabs-profileVertical" className="nav-link block font-medium text-xs leading-tight uppercase border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent"
-                  id="tabs-profile-tabVertical" data-bs-toggle="pill" data-bs-target="#tabs-profileVertical" role="tab"
-                  aria-controls="tabs-profileVertical" aria-selected="false">Translation</a>
-              </li>
-              <li className="nav-item flex-grow text-center" role="presentation">
-                <a href="#tabs-messagesVertical" className="nav-link block font-medium text-xs leading-tight uppercase border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent"
-                  id="tabs-messages-tabVertical" data-bs-toggle="pill" data-bs-target="#tabs-messagesVertical" role="tab"
-                  aria-controls="tabs-messagesVertical" aria-selected="false">Other</a>
-              </li>
-            </ul>
-            <div className="tab-content" id="tabs-tabContentVertical">
-              <div className="tab-pane fade show active" id="tabs-homeVertical" role="tabpanel"
-                aria-labelledby="tabs-home-tabVertical">
-                Tab 1 content vertical
-                <Card prefix="" filename={formValue1_1} chunkNum={Number(formValue2_1)} chunkSize={Number(formValue3_1)} />
-              </div>
-              <div className="tab-pane fade" id="tabs-profileVertical" role="tabpanel" aria-labelledby="tabs-profile-tabVertical">
-                Tab 2 content vertical
-                <Card prefix="translation" filename={formValue1_2} chunkNum={Number(formValue2_2)} chunkSize={Number(formValue3_2)} />
-              </div>
-              <div className="tab-pane fade" id="tabs-messagesVertical" role="tabpanel"
-                aria-labelledby="tabs-profile-tabVertical">
-                Tab 3 content vertical
-              </div>
+          <ul className="nav nav-tabs flex flex-col md:flex-row flex-wrap list-none border-b-0 pl-0 mb-4" id="tabs-tab"
+            role="tablist">
+            <li className="nav-item" role="presentation">
+              <a href="#tabs-original" className="nav-link block font-medium text-xs leading-tight uppercase border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent active"
+                id="tabs-original-tab" data-bs-toggle="pill" data-bs-target="#tabs-original" role="tab" aria-controls="tabs-original"
+                aria-selected="true">Original</a>
+            </li>
+            <li className="nav-item" role="presentation">
+              <a href="#tabs-translation" className="nav-link block font-medium text-xs leading-tight uppercase border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent"
+                id="tabs-translation-tab" data-bs-toggle="pill" data-bs-target="#tabs-translation" role="tab"
+                aria-controls="tabs-translation" aria-selected="false">Translation</a>
+            </li>
+
+          </ul>
+          <div className="tab-content" id="tabs-tabContent">
+            <div className="tab-pane fade show active" id="tabs-original" role="tabpanel" aria-labelledby="tabs-original-tab">
+              Tab 1 content
+              <Card prefix="" filename={formValue1_1} chunkNum={Number(formValue2_1)} chunkSize={Number(formValue3_1)} />
+            </div>
+            <div className="tab-pane fade" id="tabs-translation" role="tabpanel" aria-labelledby="tabs-translation-tab">
+              Tab 2 content
+              <Card prefix="translation" filename={formValue1_2} chunkNum={Number(formValue2_2)} chunkSize={Number(formValue3_2)} />
             </div>
           </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
